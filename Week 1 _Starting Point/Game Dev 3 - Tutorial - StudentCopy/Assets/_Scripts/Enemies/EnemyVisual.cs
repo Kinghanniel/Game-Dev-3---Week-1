04f0248afddfbe133319d1c172cb4e01c2a9847d
Kinghanniel <165713490+Kinghanniel@users.noreply.github.com>
2024-09-25 17:37:06 +0100

    added in the ship prefab and done all the coding from the project

---
+++ Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemyVisual.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVisual : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public EnemyData enemyData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = enemyData.shipSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

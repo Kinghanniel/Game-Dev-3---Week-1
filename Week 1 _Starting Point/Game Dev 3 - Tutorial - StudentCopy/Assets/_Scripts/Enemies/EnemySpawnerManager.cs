65ac1c7c139821a4af321d95674c14c4df8a9ce3
Kinghanniel <165713490+Kinghanniel@users.noreply.github.com>
2024-10-10 01:50:08 +0100

    Enemy Spawner created fully
    (Working well) ??? I don't know yet

--- Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemySpawnerManager.cs
+++ Week 1 _Starting Point/Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemySpawnerManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class EnemySpawnerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] float delayBetweenSpawns;
+    [SerializeField] int numberOfEnemiesSpawned;
+    [SerializeField] float delayBetweenWaves;
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] EnemyData[] enemyData;
+    [SerializeField] int wavesNumber;
+    private int currentWaveCount = 0;
+
+    public void SpawnLogic()
     {
-        
+        StartCoroutine(SpawnWaves());   
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnWave()
     {
+        for (int i = 0; i < numberOfEnemiesSpawned; i++)
+        {
+            int randomInterger = Random.Range(0, spawnPoints.Length -1);
+            GameObject spawnedShip = Instantiate(enemyPrefab, spawnPoints[randomInterger]);
+            spawnedShip.GetComponent<EnemyVisual>().enemyData = enemyData[currentWaveCount];
+            spawnedShip.GetComponent<EnemyMovement>().enemyData = enemyData[currentWaveCount];
+            spawnedShip.GetComponent<EnemyLife>().enemyData = enemyData[currentWaveCount];
+            yield return new WaitForSeconds(delayBetweenWaves);
+        }
+
+
         
+        currentWaveCount++;
+        if (currentWaveCount > enemyData.Length - 1)
+        {
+            currentWaveCount = 0;
+        }
+
+        yield return new WaitForSeconds(delayBetweenWaves);
+        StartCoroutine(SpawnWave());
+
     }
+
 }
